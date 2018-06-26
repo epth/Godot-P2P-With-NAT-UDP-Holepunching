bbc89a603f8959669654b4e93c2ab45ed2ebee69
mudbile <eulerspill@protonmail.com>
2018-06-26 16:58:46 +1000

    test

--- godot-server.py
+++ godot-server.py
@@ -50,7 +50,8 @@ class ServerProtocol(DatagramProtocol):
                 '__sender-name': jData['__sender-name']
             }
             print(jData['__sender-name'] + " added to server list")
-            print("global adress: " + str(jData['global-address']))
+            print("global adress0: " + str(address))
+            print("global adress1: " + str(jData['global-address']))
             #send back confirmation
             self.send('confirming-registration', jData['__sender-name'], jData['global-address'], {}, jData['password'])
 
